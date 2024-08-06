68ef59918edd87d89d5c4000e8337e165cd793e7
ricardobcsilva <166446131+ricardobcsilva@users.noreply.github.com>
2024-08-05 22:18:01 -0300

    Add files via upload

---
+++ aulas/2024-08-05/script.js
@@ -0,0 +1,58 @@
+//let nome = "Ricardo";
+
+//alert("Boa noite");
+//console.log(1+10);
+
+//alert("Olá!");
+//console.log("Ricardo");
+
+//function permitirEntrada() {
+ //   console.log("entrar");
+//}
+
+//function negarEntrada() {
+//}
+
+//function temAutorizacao() {
+
+//}
+
+//let resposta;
+//resposta = confirm("Maior de idade?");
+//console.log(resposta);
+
+//if (resposta == true) {
+//    permitirEntrada();
+//} else {
+//    let temAutorizacao;
+//    temAutorizacao = confirm("Voce tem autorizacao");
+//}
+
+//if (temAutorizacao == true) {
+ //   permitirEntrada();
+//
+//} else {
+//    negarEntrada();
+//}
+
+//for (;;) {
+//    alert(5);
+//}
+
+//let resposta;
+//resposta = confirm("Ja chegamos?");
+//console.log(resposta);
+
+//for (let contador = 0; contador <=100; contador += 2) {
+//    console.log(contador);
+//}
+
+for (let contador = 0; contador <= 100; contador += 1) {
+    if (contador % 2 == 1) {
+    console.log(contador);
+    } 
+}
+
+//for (let i = 0; i <= 100; i++) {
+//    console.log(i);
+//}
